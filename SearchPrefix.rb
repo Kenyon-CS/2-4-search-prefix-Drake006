41eddecc232829840844a775b1e0cae17d1886bb
Drake Lewis <lewis5@kenyon.edu>
2024-02-06 17:57:59 -0500

    Finished Version

--- SearchPrefix.rb
+++ SearchPrefix.rb
@@ -1,7 +1,14 @@
 def searchPrefix(arr,s)
-
+    new_array=[]
+    for i in arr
+        if i.start_with?(s)
+            new_array << i
+        end
+    end
+    return new_array
 end
 
 print(searchPrefix(["abcde", "abdf", "adeab", "abdgse", "bdefa", "bacdef"],"ab"))
+puts " "
 
 print(searchPrefix(["abcde", "abdf", "adeab", "abdgse", "bdefa", "bacdef"],"b"))
